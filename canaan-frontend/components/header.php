17b88d60dc6be35a520c8b9cd70b6cea67344a18
MR222351 <127555830+imlesly@users.noreply.github.com>
2025-05-17 23:26:19 -0600

    Update header.php
    con iconos

--- canaan-frontend/components/header.php
+++ canaan-frontend/components/header.php
@@ -2,10 +2,11 @@
 <html lang="es">
 <head>
   <meta charset="UTF-8">
-  <title>Iglesia Canaán Central</title>
+  <title>Iglesia Canaán Central </title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="../assets/css/style.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
+  
 
 </head>
 <body>
@@ -16,21 +17,19 @@
     <img src="../assets/img/logo canaan_angeles.png" style="width: 35px;" alt="logo canaan">
     <a class="navbar-brand text-warning ms-2" href="home.php">Canaán Central</a>
 
-    <!-- Botón de colapso para móviles -->
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menu">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+   
 
     <!-- Contenido colapsable -->
     <div class="collapse navbar-collapse justify-content-between" id="menu">
       
       <!-- Buscador centrado -->
-      <form class="d-flex mx-auto" style="width: 40%;">
-  <input class="form-control me-2" 
+     <form class="d-flex mx-auto" style="width: 60%;">
+  <input class="form-control me -2 w-100" 
          type="search" 
-         placeholder="Buscar" 
+         name="q"
+         placeholder="Buscar"
          aria-label="Buscar"
-         style="background-color:rgb(254, 254, 255); color: #00263e; border-color:rgb(151, 214, 255);">
+         style="background-color: rgb(254, 254, 255); color: #00263e; border-color: rgb(25, 144, 223);">
   
   <button class="btn" 
           type="submit"
@@ -39,16 +38,53 @@
   </button>
 </form>
 
-      <!-- Menú de navegación -->
-      <ul class="navbar-nav ms-auto">
-        <li class="nav-item"><a class="nav-link text-white" href="eventos.php">Eventos</a></li>
-        <li class="nav-item"><a class="nav-link text-white" href="sermones.php">Sermones</a></li>
-        <li class="nav-item"><a class="nav-link text-white" href="ministerios.php">Ministerios</a></li>
-        <li class="nav-item"><a class="nav-link text-white" href="pastores.php">Pastores</a></li>
-        <li class="nav-item"><a class="nav-link text-white" href="predicaciones.php">Predicaciones</a></li>
-        <li class="nav-item"><a class="nav-link text-white" href="contacto.php">Contacto</a></li>
-      </ul>
+ <ul>
+  </ul>
+ <ul class="navbar-nav ms-auto d-flex align-items-center gap-3">
 
+        <li class="nav-item">
+          <a class="nav-link text-white d-flex align-items-center" href="eventos.php">
+            <i class="bi bi-calendar3 me-1"></i> Eventos
+          </a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link text-white d-flex align-items-center" href="sermones.php">
+            <i class="bi bi-journal-text me-1"></i> Sermones
+          </a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link text-white d-flex align-items-center" href="ministerios.php">
+            <i class="bi bi-house-fill me-1"></i> Ministerios
+          </a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link text-white d-flex align-items-center" href="pastores.php">
+            <i class="bi bi-person-lines-fill me-1"></i> Pastores
+          </a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link text-white d-flex align-items-center" href="predicaciones.php">
+            <i class="bi bi-journal-text me-1"></i> Predicaciones
+          </a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link text-white d-flex align-items-center" href="contacto.php">
+            <i class="bi bi-telephone-fill me-1"></i> Contacto
+          </a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link text-white d-flex align-items-center" href="login.php">
+            <i class="bi bi-person-fill me-1"></i>Login
+          </a>
+        </li>
+
+      </ul>
     </div>
   </div>
 </nav>
