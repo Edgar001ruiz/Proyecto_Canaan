3fbf075b0e5e06c0d4bcaad51f55ed73e8f2f0bc
MR222351 <127555830+imlesly@users.noreply.github.com>
2025-05-17 20:13:19 -0600

    Update header.php
    diseño mejorado

--- canaan-frontend/components/header.php
+++ canaan-frontend/components/header.php
@@ -9,21 +9,47 @@
 
 </head>
 <body>
-
-<nav class="navbar navbar-expand-lg" style="background-color: #101820;">
+<nav class="navbar navbar-expand-lg" style="background-color: #00263e;">
   <div class="container">
-    <a class="navbar-brand text-warning" href="home.php">Canaán Central</a>
+    
+    <!-- Logo y nombre -->
+    <img src="../assets/img/logo canaan_angeles.png" style="width: 35px;" alt="logo canaan">
+    <a class="navbar-brand text-warning ms-2" href="home.php">Canaán Central</a>
+
+    <!-- Botón de colapso para móviles -->
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menu">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="menu">
+
+    <!-- Contenido colapsable -->
+    <div class="collapse navbar-collapse justify-content-between" id="menu">
+      
+      <!-- Buscador centrado -->
+      <form class="d-flex mx-auto" style="width: 40%;">
+  <input class="form-control me-2" 
+         type="search" 
+         placeholder="Buscar" 
+         aria-label="Buscar"
+         style="background-color:rgb(254, 254, 255); color: #00263e; border-color:rgb(151, 214, 255);">
+  
+  <button class="btn" 
+          type="submit"
+          style="background-color: #ffc107; color: #00263e; border-color: #00263e;">
+    Buscar
+  </button>
+</form>
+
+      <!-- Menú de navegación -->
       <ul class="navbar-nav ms-auto">
         <li class="nav-item"><a class="nav-link text-white" href="eventos.php">Eventos</a></li>
         <li class="nav-item"><a class="nav-link text-white" href="sermones.php">Sermones</a></li>
         <li class="nav-item"><a class="nav-link text-white" href="ministerios.php">Ministerios</a></li>
         <li class="nav-item"><a class="nav-link text-white" href="pastores.php">Pastores</a></li>
+        <li class="nav-item"><a class="nav-link text-white" href="predicaciones.php">Predicaciones</a></li>
         <li class="nav-item"><a class="nav-link text-white" href="contacto.php">Contacto</a></li>
       </ul>
+
     </div>
   </div>
 </nav>
+
