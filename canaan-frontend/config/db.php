ec91b8605283664cb5042d73e9bad2cf996c2751
EdgarMelendez06 <GM240279@alumno.udb.edu.org.sv>
2025-05-19 19:08:38 -0600

    DockerFile

--- canaan-frontend/config/db.php
+++ canaan-frontend/config/db.php
@@ -1,11 +1,12 @@
 <?php
-$host = 'localhost';
-$db = 'canaan_db';
-$user = 'root';
-$pass = '';
+$host = getenv('MYSQL_HOST') ?: 'localhost';
+$db = getenv('MYSQL_DATABASE') ?: 'canaan_db';
+$user = getenv('MYSQL_USER') ?: 'root';
+$pass = getenv('MYSQL_PASSWORD') ?: '';
+$port = 3306;
 
 try {
-$pdo = new PDO("mysql:host=$host;port=3307;dbname=$db;charset=utf8", $user, $pass);
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$db;charset=utf8", $user, $pass);
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
   die("Error en la conexión: " . $e->getMessage());
